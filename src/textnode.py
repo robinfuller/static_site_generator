4bbf735390cf363afd2a98e7893c0b4a7c999459
robinfuller <robinc.fuller@gmail.com>
2024-06-10 20:58:25 -0600

    clean up __eq__ in textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -5,7 +5,11 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type 
+            and self.url == other.url
+        )
     
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
