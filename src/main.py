cef64fffe14f64741da3427a2b267fa7c780d473
robinfuller <robin.cfuller@gmail.com>
2024-06-12 21:58:16 -0600

    import textnode_to_html_node to main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from textnode import TextNode
-import htmlnode
+from textnode import TextNode, textnode_to_html_node
+
 
 
 def main():
     supercombo = TextNode("TextNode, yay", "link", "https://www.supercombo.gg")
     print(supercombo)
-    supercombo_converted = htmlnode.textnode_to_html_node(supercombo)
+    supercombo_converted = textnode_to_html_node(supercombo)
     print(supercombo_converted.to_html())
 
 main()
